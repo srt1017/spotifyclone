6c2f9d66053163a8d7ac79da556af025ce6f4d9b
srt1017 <omarfarouq107@gmail.com>
2023-07-23 16:13:42 -0500

    Built Modal component for auth

--- spotifyclone/providers/ModalProvider.tsx
+++ spotifyclone/providers/ModalProvider.tsx
@@ -14,7 +14,9 @@ const ModalProvider = () => {
 
   return (
   <>
-    <Modal />
+    <Modal title="Test Modal" description="Test Description" isOpen onChange={() => {}}>
+       Test Children 
+    </Modal>
   </>
   );
 };
